57b57707785f15da5e24c1edfa237eba269a91e7
Neil Berkman <neil@xuku.com>
2025-07-18 23:05:29 -0700

    Update Draggy to v1.1.0

--- Casks/draggy.rb
+++ Casks/draggy.rb
@@ -1,6 +1,6 @@
 cask "draggy" do
-  version "0.9.0"
-  sha256 "60bc08dc0f8e557ca39d4708d3c3f5677c21ee3a87e160c37d54846e385272e8"
+  version "1.1.0"
+  sha256 "97dbef13b7c3db24c930677b67afd03d21bc2bbc3df8c81f22445b7ee335f929"
 
   url "https://github.com/neilberkman/clippy/releases/download/v#{version}/Draggy.app.zip"
   name "Draggy"
@@ -13,6 +13,13 @@ cask "draggy" do
   end
 
   app "Draggy.app"
+
+  # Remove quarantine attribute to bypass Gatekeeper
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Draggy.app"],
+                   sudo: false
+  end
 
   zap trash: [
     "~/Library/Preferences/com.clippy.draggy.plist",
